248412660c826b14c7d3c747d7831da41bbfecd6
Vadim Rutkovsky <vrutkovs@redhat.com>
2025-02-28 10:02:17 +0100

    HACK certrotation: ignore optimistic lock error

--- vendor/github.com/openshift/library-go/pkg/operator/certrotation/target.go
+++ vendor/github.com/openshift/library-go/pkg/operator/certrotation/target.go
@@ -18,6 +18,7 @@ import (
 	"github.com/openshift/library-go/pkg/crypto"
 	"github.com/openshift/library-go/pkg/operator/events"
 	"github.com/openshift/library-go/pkg/operator/resource/resourcehelper"
+	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
 	corev1informers "k8s.io/client-go/informers/core/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	corev1listers "k8s.io/client-go/listers/core/v1"
@@ -140,6 +141,10 @@ func (c RotatedSelfSignedCertKeySecret) EnsureTargetCertKeyPair(ctx context.Cont
 		targetCertKeyPairSecret = actualTargetCertKeyPairSecret
 	} else if updateRequired {
 		actualTargetCertKeyPairSecret, err := c.Client.Secrets(c.Namespace).Update(ctx, targetCertKeyPairSecret, metav1.UpdateOptions{})
+		if strings.Contains(err.Error(), genericregistry.OptimisticLockErrorMsg) {
+			// ignore error if its attempting to update outdated version of the secret
+			return nil, nil
+		}
 		resourcehelper.ReportUpdateEvent(c.EventRecorder, actualTargetCertKeyPairSecret, err)
 		if err != nil {
 			return nil, err
